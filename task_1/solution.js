3e25b215adb2db2dfe1660990fbff3622d665def
Gridina Ekaterina <83913421+Kettig@users.noreply.github.com>
2021-05-11 22:47:27 +0300

    Task 1 done

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,6 +1,13 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
+    let message;
+    message = title + " за ";
+    message = message + price;
+    message = message + " теперь в корзине!"
+    
+    console.log(message);
+
 
     // Задание №1.1. Оповещение о добавлении в корзину
 
@@ -23,6 +30,8 @@ function addInCartCountChange(value) {
 
     // Задание №1.2. Увеличение количества в корзине
     let oldValue = value;
+    let newValue;
+    newValue = oldValue+1;
 
     // создайте переменную newValue
     // присвойте ей значение oldValue
@@ -36,6 +45,11 @@ function addInCartCountChange(value) {
 function addInCartSumChange(sum, delta) {
     let oldSum = sum;
     let difference = delta;
+    let newSum;
+    newSum = sum + delta;
+    let newSumText = newSum + " ₽";
+    newSumText = `${newSum} ₽`;
+    
 
     // Задание №1.3. Увеличение суммы в корзине
 
