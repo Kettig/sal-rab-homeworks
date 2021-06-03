a4140c69e8fc22585ebfe49b5eae6aa47324513d
Gridina Ekaterina <83913421+Kettig@users.noreply.github.com>
2021-06-03 21:53:04 +0300

    fol

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,18 +34,21 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: "", order: {}, goods: []};
 
     let countOfGoods = goods.length;
+    
+    for (let i = 0; i < countOfGoods; i += 1) {
+        let goodsNew = {'title': goods[i].title, 'count': goods[i].count};
+        data.goods.push(goodsNew);
+    }    
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
+    data.client = name + ' ' + phone;
+    data.order.address = 'ул. ' + address.street + ', дом ' + address.house + ', ' + address.entrance + ' подъезд, ' + address.floor + ' этаж, кв ' + address.flat;
+    data.order.sum = sum;
+    
+     //ул. Академика Королёва, дом 12, 1 подъезд, 15 этаж, кв 253
+    //data.client = 'Иван';
 
     let jsonData = JSON.stringify(data);
 
